1499b517c7fffb87bc643b4a81f1e43e0e78f6d3
森尾　優菜 <>
2024-03-15 16:07:50 +0900

    test

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import { css } from "../../styled-system/css";
+import { center, flex } from "../../styled-system/patterns";
+
+
+export default function Home() {
+  return (
+    <div>hello</div>
+  )
+}
